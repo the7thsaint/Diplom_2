f836b4fccb604e1449a05bc67ef4ef5b6e819345
Pavel Vinogradov <pav.vinogradow1@yandex.ru>
2023-05-25 08:44:20 +0300

    create order class

---
+++ src/main/java/order_data/OrderInfo.java
@@ -0,0 +1,13 @@
+package order_data;
+
+public class OrderInfo {
+    private String[] ingredients;
+    private String _id;
+    private String status;
+    private String number;
+
+    public OrderInfo(){
+
+    }
+
+}
